fbf3907d81fa3418be79d04844fb5767a6df9df3
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-10-06 18:59:00 +0700

    Create AdminSideBar

--- client/src/components/admin-view/layout.jsx
+++ client/src/components/admin-view/layout.jsx
@@ -9,7 +9,7 @@ export default function AdminLayout() {
 
 	return (
 		<div className='flex min-h-screen w-full'>
-			<AdminSideBar />
+			<AdminSideBar open={openSidebar} setOpen={setOpenSidebar} />
 			<div className='flex flex-1 flex-col'>
 				<AdminHeader setOpen={setOpenSidebar} />
 				<main className='flex-1 flex-col flex bg-muted/40 p-4 md:p-6'>
