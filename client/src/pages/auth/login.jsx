79bfef6c3b7dd91e6835064ea7d4bc74b3a785c5
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-10-06 11:02:01 +0700

    Create AuthLogin

--- client/src/pages/auth/login.jsx
+++ client/src/pages/auth/login.jsx
@@ -1,3 +1,40 @@
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+
+import { loginFormControls } from '@/config'
+import CommonForm from '@/components/common/form'
+
+const initialState = {
+	email: '',
+	password: '',
+}
+
 export default function AuthLogin() {
-	return <div>Login</div>
+	const [formData, setFormData] = useState(initialState)
+
+	return (
+		<div className='mx-auto w-full max-w-md space-y-6'>
+			<div className='text-center'>
+				<h1 className='text-3xl font-bold tracking-tight text-foreground'>
+					Login to your account
+				</h1>
+				<p className='mt-2'>
+					Don't have an account
+					<Link
+						className='font-medium ml-2 text-primary hover:underline'
+						to='/auth/register'
+					>
+						Register
+					</Link>
+				</p>
+			</div>
+			<CommonForm
+				formControls={loginFormControls}
+				buttonText={'Login'}
+				formData={formData}
+				setFormData={setFormData}
+				onSubmit={() => {}}
+			/>
+		</div>
+	)
 }
