6144a9b37ecac4bd2c9a53b6f67e627bb0d62dcb
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-10-08 20:14:20 +0700

    Create ShoppingHome

--- client/src/components/shopping-view/header.jsx
+++ client/src/components/shopping-view/header.jsx
@@ -1,5 +1,10 @@
 import { useEffect, useState } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
+import {
+	Link,
+	useLocation,
+	useNavigate,
+	useSearchParams,
+} from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import { HousePlug, LogOut, Menu, ShoppingCart, UserCog } from 'lucide-react'
 
@@ -21,10 +26,38 @@ import {
 } from '../ui/dropdown-menu'
 
 function MenuItems() {
+	const navigate = useNavigate()
+	const location = useLocation()
+	const [searchParams, setSearchParams] = useSearchParams()
+
+	function handleNavigate(getCurrentMenuItem) {
+		sessionStorage.removeItem('filters')
+		const currentFilter =
+			getCurrentMenuItem.id !== 'home' &&
+			getCurrentMenuItem.id !== 'products' &&
+			getCurrentMenuItem.id !== 'search'
+				? {
+						category: [getCurrentMenuItem.id],
+				  }
+				: null
+
+		sessionStorage.setItem('filters', JSON.stringify(currentFilter))
+
+		location.pathname.includes('listing') && currentFilter !== null
+			? setSearchParams(
+					new URLSearchParams(`?category=${getCurrentMenuItem.id}`)
+			  )
+			: navigate(getCurrentMenuItem.path)
+	}
+
 	return (
 		<nav className='flex flex-col mb-3 lg:mb-0 lg:items-center gap-6 lg:flex-row'>
 			{shoppingViewHeaderMenuItems.map((menuItem) => (
-				<Label className='text-sm font-medium cursor-pointer' key={menuItem.id}>
+				<Label
+					onClick={() => handleNavigate(menuItem)}
+					className='text-sm font-medium cursor-pointer'
+					key={menuItem.id}
+				>
 					{menuItem.label}
 				</Label>
 			))}
