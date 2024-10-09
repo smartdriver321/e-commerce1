c6804215ef56f3a85464f16a1e23d2c9b4179394
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-10-09 11:25:27 +0700

    Create admin order API

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -2,6 +2,8 @@ import { configureStore } from '@reduxjs/toolkit'
 
 import authSlice from './auth-slice'
 import adminProductsSlice from './admin/products-slice'
+import adminOrderSlice from './admin/order-slice'
+
 import shopProductsSlice from './shop/products-slice'
 import shopCartSlice from './shop/cart-slice'
 import shopAddressSlice from './shop/address-slice'
@@ -11,6 +13,8 @@ const store = configureStore({
 		auth: authSlice,
 
 		adminProducts: adminProductsSlice,
+		adminOrder: adminOrderSlice,
+
 		shopProducts: shopProductsSlice,
 		shopCart: shopCartSlice,
 		shopAddress: shopAddressSlice,
--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ require('dotenv').config()
 
 const authRouter = require('./routes/auth/auth-routes')
 const adminProductsRouter = require('./routes/admin/products-routes')
+const adminOrderRouter = require('./routes/admin/order-routes')
 const shopProductsRouter = require('./routes/shop/products-routes')
 const shopCartRouter = require('./routes/shop/cart-routes')
 const shopAddressRouter = require('./routes/shop/address-routes')
@@ -33,6 +34,8 @@ app.use(
 
 app.use('/api/auth', authRouter)
 app.use('/api/admin/products', adminProductsRouter)
+app.use('/api/admin/orders', adminOrderRouter)
+
 app.use('/api/shop/products', shopProductsRouter)
 app.use('/api/shop/cart', shopCartRouter)
 app.use('/api/shop/address', shopAddressRouter)
